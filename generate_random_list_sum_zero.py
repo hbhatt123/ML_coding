f03f0126a0ec875d509f95a33e3479580ba4492e
Himani Bhatt <32537184+hbhatt123@users.noreply.github.com>
2024-07-12 18:02:03 +0530

    Update generate_random_list_sum_zero.py

--- generate_random_list_sum_zero.py
+++ generate_random_list_sum_zero.py
@@ -2,3 +2,12 @@ N=4
 l = random.sample(range(-10, 10), k=3)
 print(l)
 print(l + [-sum(l)])
+
+def sumzero(n):
+    
+    ls = []
+    sum = 0
+    ls = [ i for i in range(1,n//2+1)] # generate half of N numberes
+    ls += [-i for i in ls] # generate half of opposite sign numbers
+    print(ls)
+    return ls if n%2 ==0 else ls + [0]  return list if N is even otherwise add 0 to the list(in odd)
